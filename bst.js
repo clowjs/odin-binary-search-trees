668d6309e2be7780bd6c1f41a0b8fb454fade284
Sergio Santos <64509249+clowjs@users.noreply.github.com>
2024-03-15 15:12:22 -0300

    feat: add deleteItem()

--- bst.js
+++ bst.js
@@ -65,9 +65,42 @@ class Tree {
       }
     }
   }
+
+  deleteItem(data, root = this.root) {
+    if (!root) return root;
+
+    if (data < root.data) {
+      root.left = this.deleteItem(data, root.left);
+    } else if (data > root.data) {
+      root.right = this.deleteItem(data, root.right);
+    } else {
+      if (!root.left) {
+        return root.right;
+      } else if (!root.right) {
+        return root.left;
+      }
+
+      root.data = this.minValue(root.right);
+      root.right = this.deleteItem(root.data, root.right);
+    }
+
+    return root;
+  }
+
+  minValue(node) {
+    let current = node;
+
+    while (current.left) {
+      current = current.left;
+    }
+
+    return current.data;
+  }
+
+  
 }
 
-const array = [5, 4, 6, 3, 2, 1, 0];
+const array = [5, 4, 6, 3, 2, 1, 0, 30, 25, 39, 99];
 const tree = new Tree(array);
 
 tree.prettyPrint();
@@ -76,4 +109,10 @@ tree.insert(7);
 tree.insert(-7);
 tree.insert(11);
 
+tree.prettyPrint();
+
+tree.deleteItem(5);
+tree.prettyPrint();
+
+tree.deleteItem(25);
 tree.prettyPrint();
